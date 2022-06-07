06906019db164a6646541114a2592fa139ec1aaa
Gaurav Kumar <gauravkumar15021999@gmail.com>
2022-06-08 01:18:33 +0530

    Add Binary Search

---
+++ dsa/algorithms/searching/BinarySearch.java
@@ -0,0 +1,65 @@
+package dsa.algorithms.searching;
+
+public class BinarySearch {
+    public static int findInAsc(int[] array, int target) {
+        int start = 0, end = array.length - 1;
+        while (start <= end) {
+            // int mid = (start+end)/2; // Do not use this formula for mid becasuse in some
+            // case this may break and cross the integer limit
+            int mid = start + (end - start) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int findInDsc(int[] array, int target) {
+        int start = 0, end = array.length - 1;
+        while (start <= end) {
+            // int mid = (start+end)/2; // Do not use this formula for mid becasuse in some
+            // case this may break and cross the integer limit
+            int mid = start + (end - start) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int orderAgnostic(int[] array, int target) {
+        int start = 0, end = array.length - 1;
+        boolean isAsc = (array[start] < array[end]);
+        while (start <= end) {
+            // int mid = (start+end)/2; // Do not use this formula for mid becasuse in some
+            // case this may break and cross the integer limit
+            int mid = start + (end - start) / 2;
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (array[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (array[mid] < target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
